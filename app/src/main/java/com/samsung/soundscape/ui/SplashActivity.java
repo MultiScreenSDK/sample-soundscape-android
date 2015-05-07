dd58cd3e63864153d64126bd57fe0ac829ed1175
Patrick Lin <patrick.lin@samsung.com>
2015-05-06 17:20:33 -0700

    * Add splash layout + activity skeleton * Initial connect_activity layout

---
+++ app/src/main/java/com/samsung/soundscape/ui/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.samsung.soundscape.ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.samsung.soundscape.R;
+
+/**
+ * Created by plin on 5/6/15.
+ */
+public class SplashActivity extends Activity{
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash_activity);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, getResources().getInteger(R.integer.splash_timeout));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+}
